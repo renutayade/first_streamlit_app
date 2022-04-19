8900661ee623b89b81e4cf3dfa9bac9bb13a40b2
renutayade <103992582+renutayade@users.noreply.github.com>
2022-04-19 21:22:19 +0200

    Update streamlit_app.py
    Adding Json format to fruitvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on page
 streamlit.dataframe(fruits_to_show)
 
+# New Section for fruitvice API response
+streamlit.header('Fruitvice Fruit Advice!')
 fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruitvice_response)
+streamlit.text(fruitvice_response.json())
